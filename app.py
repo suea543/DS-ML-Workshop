188cd9527dc33cae747f1b771aa3241847da8180
suea543 <sueasakul2543@gmail.com>
2026-05-22 15:57:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,10 @@ st.set_page_config(page_title="MyApp", layout="wide")
 
 st.title("🏠 หน้าหลัก ")
 st.write("### Boot Camp: Data Science and Machine Learning")
+st.markdown(''':rainbow [ฮั่นแหน่]''')
 st.info("7 Day Intensive Hands-on Workshop")
-st.write("อิ๊ววววววววววววว")
-st.write("##### Day 1: การจัดการข้อมูลพื้นฐานและโครงสร้างข้อมูลด้วย Python")
+st.write("ต๊ะเอ๋")
+#st.write("##### Day 1: การจัดการข้อมูลพื้นฐานและโครงสร้างข้อมูลด้วย Python")
 
 if st.button("💰 ระบบคำนวณส่วนลดตามยอดซื้อ"):
     st.switch_page("pages/app1_discount_calc.py")
@@ -14,7 +15,7 @@ elif st.button("💰 การทำความสะอาดข้อมู�
     st.switch_page("pages/dark_clean_อืออ.py")
 elif st.button("💰 การทำความสะอาดข้อมมูลของอิ๊ววววววว"):
     st.switch_page("pages/clean_อิ๊ววววววว.py")
-elif st.button("💰 การทำความสะอาดข้อมมูลของclean_app"):
+"""elif st.button("💰 การทำความสะอาดข้อมมูลของclean_app"):
     st.switch_page("pages/clean_app.py")
 elif st.button("💰 การทำความสะอาดข้อมมูลของอือ"):
-    st.switch_page("pages/enhanced_clean_อือ.py")
+    st.switch_page("pages/enhanced_clean_อือ.py")"""
