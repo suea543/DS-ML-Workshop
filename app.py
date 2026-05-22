ff9b53cdc18d1945311ff847d873cd7dd4995674
suea543 <sueasakul2543@gmail.com>
2026-05-22 16:28:43 +0700

    Update app.py

--- app.py
+++ app.py
@@ -14,9 +14,9 @@ if st.button("💰 ระบบคำนวณส่วนลดตามยอ�
     st.switch_page("pages/app1_discount_calc.py")
 elif st.button("💰 การทำความสะอาดข้อมูลของอืออ"):
     st.switch_page("pages/dark_clean_อืออ.py")
-"""elif st.button("💰 การทำความสะอาดข้อมมูลของอิ๊ววววววว"):
-    st.switch_page("pages/clean_อิ๊ววววววว.py")
-elif st.button("💰 การทำความสะอาดข้อมมูลของclean_app"):
+elif st.button("💰 การทำความสะอาดข้อมมูลของnuch"):
+    st.switch_page("pages/graveyard_cleaner_nuch.py")
+"""elif st.button("💰 การทำความสะอาดข้อมมูลของclean_app"):
     st.switch_page("pages/clean_app.py")
 elif st.button("💰 การทำความสะอาดข้อมมูลของอือ"):
     st.switch_page("pages/enhanced_clean_อือ.py")"""
