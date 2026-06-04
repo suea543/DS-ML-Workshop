166fc46c431e7f7bf380d6e0b44589d5728833d3
suea543 <sueasakul2543@gmail.com>
2026-06-04 11:41:38 +0700

    Update app.py

--- app.py
+++ app.py
@@ -23,8 +23,8 @@ st.write("### 👻 Data Science and Machine Learning 👻")
 st.info("☠️🐦‍⬛🪓 7 Day Intensive Hands-on Workshop 💀✝️🔪")
 
 # --- Navigation Buttons (Theme-adjusted text) ---
-col1, col2, col3, col4, col5,col6 = st.columns(6)
-
+col1, col2, col3 = st.columns(3)
+col4, col5, col6 = st.columns(3)
 with col1:
     if st.button("💰 ระบบคำนวณส่วนลดตามยอดซื้อ"): # Button 1
         st.switch_page("pages/app1_discount_calc.py")
